87b71bf9a6bbcf08ffb51cd3817d0b3ae134306d
imendoza03 <ivanmendoza@gmail.com>
2018-02-21 15:28:05 +0100

    Addition of default time_stamp

--- SQL/Exercice1/myExo1.sql
+++ SQL/Exercice1/myExo1.sql
@@ -1,6 +1,6 @@
-create database test default character set= utf8 collate= utf8_bin;
+create database test default character set utf8 collate utf8_bin;
 use test;
 create table roles(
 id int unsigned not null auto_increment primary key,
-createdAt datetime not null,
+createdAt datetime not null default current_timestamp,
 label varchar(50) null);
