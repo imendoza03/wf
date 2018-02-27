eb43085ed105ed627a569d5b35e13c414aacae2a
imendoza03 <ivanmendoza@gmail.com>
2018-02-27 15:39:43 +0100

    php exercise 3 correction

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,29 @@
 
 $input;
 $winner;
+$aResult= 0;
+$bResult = 0;
+
+foreach ($input as $deck) {
+    list($player1, $player2) = $deck;
+//     $player1 = $deck[0];
+//     $player2 = $deck[1];
+    
+    if($player1 > $player2) {
+        $aResult++;
+    }elseif($player2 > $player1){
+        $bResult++;
+    }
+}
+
+echo "points of A: " . $aResult;
+echo "points of B: " . $bResult;
+
+if($aResult > $bResult){
+    $winner = 'A';
+} else {
+    $winner = 'B';
+}
+
+
+
