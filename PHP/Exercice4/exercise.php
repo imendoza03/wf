8703516e2634a20ebb3c33933a8adcd4eb347ff6
imendoza03 <ivanmendoza@gmail.com>
2018-02-27 17:02:45 +0100

    first steps towards correction

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,13 @@
 <?php
+$allDays = [];
+
+
+function getAllMondaysOfMonth($year, $month) {
+    $format = 'D d, M Y';
+    $date = DateTime::createFromFormat($format, $year);
+    $allDays[] = $date;
+    
+    return $allDays;    
+}
+
+echo getAllMondaysOfMonth(2011, 12);
