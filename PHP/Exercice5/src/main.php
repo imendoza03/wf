b90adf6e8c723f3346af30d8006c190da09334e8
imendoza03 <ivanmendoza@gmail.com>
2018-02-28 12:53:16 +0100

    namespace correction

--- PHP/Exercice5/src/main.php
+++ PHP/Exercice5/src/main.php
@@ -1,10 +1,10 @@
 <?php
-
+echo __DIR__;
 require_once __DIR__.'/DimensionalMath/Distance/DistanceCalculation.php';
 require_once __DIR__.'/DimensionalMath/Vector/AngleCalculation.php';
 
 use function DimensionalMath\Distance\threeDimensionDistance;
-use DimensionalMath\Vector\getVectorAngle;
+use function DimensionalMath\Vector\getVectorAngle as vectorAngle;
 
 $distance = threeDimensionDistance(
     [1, 1, 1],
