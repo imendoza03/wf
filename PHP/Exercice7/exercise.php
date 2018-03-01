ee148a11ef7c04b5065e2b3e153f928f4ee29164
imendoza03 <ivanmendoza@gmail.com>
2018-03-01 17:10:03 +0100

    correction exercise 7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,37 @@
 <?php
+declare(strict_types = 1);
+function divide(int $number, int $divisor) : int{
+    if ($divisor !== 0) {
+        $result = $number/$divisor;
+        return $result; 
+    } else{
+        throw new RuntimeException('Division by 0 is not allowed');
+    }
+    
+}
+
+function arrayDivide(array $values, int $divisor):array{
+    
+    try {
+        for ($i= 0; $i < count($values); $i++){
+            $result = divide($values[$i], $divisor);
+            return $result;
+        }
+        
+        
+    } catch (RuntimeException $e) {
+        echo 'jaja';
+        return $values;
+    }
+    
+//     var_dump($result);
+    
+}
+
+try {
+    divide(2,0);
+} catch (RuntimeException $e) {
+    echo $e->getMessage();
+}
+
+arrayDivide([1,2,3], 0);
